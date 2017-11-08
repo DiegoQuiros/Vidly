60fefd14d312160f8933d7875e48b27779b4d78f
Diego Quiros <DiegoMartinQuiros@gmail.com>
2017-11-08 07:08:02 -0800

    Finished Excercise 2

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -39,7 +39,7 @@ namespace Vidly.Controllers
         [Route("Customers/Details/{id}")]
         public ActionResult Details(int id)
         {
-            var customer = _context.Customers.FirstOrDefault(c => c.Id == id);
+            var customer = _context.Customers.Include(c => c.MembershipType).FirstOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -12,6 +12,8 @@ namespace Vidly.Models
         [Required] // Name can't be null
         [StringLength(255)]
         public string Name { get; set; }
+        [DisplayFormat(DataFormatString ="{0:M/d/yyyy}")]
+        public DateTime? BirthDate { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
