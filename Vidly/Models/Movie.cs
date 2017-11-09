1fc464aaa30e186b4e7abeda37a4830a74243466
Diego Quiros <DiegoMartinQuiros@gmail.com>
2017-11-09 07:03:01 -0800

    Added movie fields

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
@@ -9,5 +10,11 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Genre { get; set; }
+        [DisplayFormat(DataFormatString ="0:ddd MMM dd, yyyy")]
+        public DateTime ReleaseDate { get; set; }
+        [DisplayFormat(DataFormatString ="0:ddd MMM dd, yyyy")]
+        public DateTime DateAdded { get; set; }
+        public byte NumberInStock { get; set; }
     }
 }
