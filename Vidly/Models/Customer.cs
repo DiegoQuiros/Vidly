92a113359c6a32994a428b8f7ea255cac4416855
Diego Quiros <DiegoMartinQuiros@gmail.com>
2017-11-14 06:30:59 -0800

    Lecture 45 finished

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,13 +9,20 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+
         [Required] // Name can't be null
         [StringLength(255)]
         public string Name { get; set; }
+
         [DisplayFormat(DataFormatString ="{0:M/d/yyyy}")]
+        [Display(Name = "Date of Birth")]
         public DateTime? BirthDate { get; set; }
+
         public bool IsSubscribedToNewsletter { get; set; }
+
         public MembershipType MembershipType { get; set; }
+
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
     }
 }
