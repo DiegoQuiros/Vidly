b1aaa18a8f5c8183676f427f6c8350ef43f1a8ba
Diego Quiros <DiegoMartinQuiros@gmail.com>
2017-12-06 06:25:44 -0800

    MemoryCache Example

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
 using System.Data.Entity;
+using System.Runtime.Caching;
 
 namespace Vidly.Controllers
 {
@@ -69,6 +70,12 @@ namespace Vidly.Controllers
         // GET: Customers/Index
         public ViewResult Index()
         {
+            if (MemoryCache.Default["Genres"] == null)
+            {
+                MemoryCache.Default["Genres"] = _context.Genres.ToList();
+            }
+
+            var genres = MemoryCache.Default["Genres"] as IEnumerable<Genre>;
             return View();
         }
 
